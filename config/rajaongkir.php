f9ff049f534c633ac5ffe53de0b882761f0dab99
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2025-10-04 13:21:00 +0700

    chore: fix typo

--- config/rajaongkir.php
+++ config/rajaongkir.php
@@ -5,5 +5,5 @@ return [
     'api_key' => env('RAJAONGKIR_API_KEY'),
     'base_url' => env('RAJAONGKIR_BASE_URL', 'https://rajaongkir.komerce.id/api/v1'),
     'cost_cache_duration' => env('RAJAONGKIR_COST_CACHE_DURATION', 60), // in minutes
-    'location_cache_duration' => env('RAJAONGKIR_LOCATION_CACHE_DURATION', 1440), // in minutes (1 day
+    'location_cache_duration' => env('RAJAONGKIR_LOCATION_CACHE_DURATION', 1440), // in minutes (1 day)
 ];
